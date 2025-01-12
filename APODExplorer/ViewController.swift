8f4738bfc1f051d66c3cb25bebf8617cc93df6e7
Mikkel Allison <mikkelallison@Mikkels-Laptop.local>
2025-01-12 16:03:45 +0000

    Initial commit

---
+++ APODExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APODExplorer
+//
+//  Created by Mikkel Allison on 12/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ APODExplorerTests/APODExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  APODExplorerTests.swift
+//  APODExplorerTests
+//
+//  Created by Mikkel Allison on 12/01/2025.
+//
+
+import Testing
+@testable import APODExplorer
+
+struct APODExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
